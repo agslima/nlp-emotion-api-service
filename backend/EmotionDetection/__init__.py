ef86cdfd8d729dd6a99ba4b7cb2383ff1b2eb5a5
Agnaldo Silva Lima <a.agnaldosilva@gmail.com>
2025-12-05 04:11:54 +0000

    modified:   backend/EmotionDetection/__init__.py

--- backend/EmotionDetection/__init__.py
+++ backend/EmotionDetection/__init__.py
@@ -1,3 +1,10 @@
+"""
+Este módulo inicializa o pacote EmotionDetection.
+Ele expõe a função emotion_detector para uso externo.
+"""
+
+# pylint: disable=invalid-name
+
 from .emotion_detection import emotion_detector
 
 __all__ = ["emotion_detector"]
